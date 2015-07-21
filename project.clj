9b1fd785d493d54c02fbf46cf060890df1613349
Chris Dean <ctdean@sokitomi.com>
2015-07-20 20:42:36 -0700

    Simple cron jobs

--- project.clj
+++ project.clj
@@ -11,22 +11,5 @@
      [org.slf4j/slf4j-log4j12 "1.7.7"]
      ]
     :dev {
-          :dependencies [cider/cider-nrepl "0.9.1"]
-          :repl-options {:nrepl-middleware
-                         [cider.nrepl.middleware.apropos/wrap-apropos
-                          cider.nrepl.middleware.classpath/wrap-classpath
-                          cider.nrepl.middleware.complete/wrap-complete
-                          cider.nrepl.middleware.debug/wrap-debug
-                          cider.nrepl.middleware.format/wrap-format
-                          cider.nrepl.middleware.info/wrap-info
-                          cider.nrepl.middleware.inspect/wrap-inspect
-                          cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                          cider.nrepl.middleware.ns/wrap-ns
-                          cider.nrepl.middleware.pprint/wrap-pprint
-                          cider.nrepl.middleware.refresh/wrap-refresh
-                          cider.nrepl.middleware.resource/wrap-resource
-                          cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                          cider.nrepl.middleware.test/wrap-test
-                          cider.nrepl.middleware.trace/wrap-trace
-                          cider.nrepl.middleware.undef/wrap-undef]}}
+          :dependencies [cider/cider-nrepl "0.9.1"]}
 )
--- src/backtick/core.clj
+++ src/backtick/core.clj
@@ -2,14 +2,18 @@
   "@ctdean"
   (:require
    [clams.conf :as conf]
+   [clj-time.coerce :as coerce]
+   [clj-time.core :as time]
    [clojure.core.async :as async :refer [chan alts! alts!!
-                                         <!! >!! <! >! close! go go-loop timeout]]
+                                         <!! >!! <! >! close!
+                                         go go-loop timeout]]
    [clojure.pprint :as pprint]
    [clojure.tools.logging :as log]
    [iter.core :refer [iter iter*]]
    [monger.collection :as mc]
-   [monger.operators :refer [$set $inc]]
    [monger.core :as mg]
+   monger.joda-time
+   [monger.operators :refer [$set $setOnInsert $inc $lt $ne]]
    )
   (:import (java.util.concurrent Executors TimeUnit)))
 
@@ -18,10 +22,14 @@
 ;;;
 
 (defn load-config []
-  (let [cf (merge {:coll "bt_queue"
-                   :poll-ms 1000
-                   :timeout 5000}
-                  (conf/get :backtick))
+  (let [cf {:mongo-url          (conf/get :bt-mongo-url)
+            :coll               (conf/get :bt-coll           "bt_queue")
+            :cron               (conf/get :bt-cron           "bt_cron")
+            :poll-ms            (conf/get :bt-poll-ms        1000)
+            :cron-ms            (conf/get :bt-cron-ms        (* 60 1000))
+            :timeout            (conf/get :bt-timeout        5000)
+            :cron-window-ms     (conf/get :bt-cron-window-ms 1000)
+            }
         mongo-cnx (mg/connect-via-uri (:mongo-url cf))
         conn (:conn mongo-cnx)
         db (:db mongo-cnx)]
@@ -30,18 +38,14 @@
 (def master-cf (load-config))
 
 ;;;
-;;; Runner
-;;;
-
-;;;
 ;;; Workers
 ;;;
 
-(def workers (atom {}))
+(def ^:private workers (atom {}))
 
-(def factory-counter (atom 0))
+(def ^:private factory-counter (atom 0))
 
-(defn make-thread-factory []
+(defn- make-thread-factory []
   (let [thread-counter (atom 0)
         name (swap! factory-counter inc)]
     (reify java.util.concurrent.ThreadFactory
@@ -50,7 +54,7 @@
                               name
                               (swap! thread-counter inc)))))))
 
-(defn submit-worker [pool ch msg]
+(defn- submit-worker [pool ch msg]
   (log/infof "Running job")
   (let [{worker :worker data :data} msg
         f (@workers worker)]
@@ -64,9 +68,30 @@
 (defn register [name f]
   (swap! workers assoc name f))
 
+(defn register-cron
+  "Register or re-register a scheduled job."
+  [name interval f]
+  (register name (fn [_] (f)))
+  ;; There are three cases here:
+  ;;  1.  A new job => We should insert it into the DB
+  ;;  2.  An existing job, with a new interval => Change the next and
+  ;;      interval fields.
+  ;;  3.  A existing job, with the same interval => Do nothing
+  (let [t (time/now)]
+    (when (not (mc/find-and-modify (:db master-cf)  ; Change the interval if needed
+                                   (:cron master-cf)
+                                   {:worker name :interval {$ne interval}}
+                                   {$set {:next t :interval interval}}
+                                   {:new true}))
+      (mc/find-and-modify (:db master-cf) ; Insert new job if needed
+                          (:cron master-cf)
+                          {:worker name}
+                          {$setOnInsert {:worker name :next t :interval interval :x 3}}
+                          {:upsert true}))))
+
 (defn perform [name args]
   (if (not (@workers name))
-      (log/errorf "No worker %s registered, discarding job" name)
+      (log/errorf "No worker %s registered, not queuing job" name)
       (mc/insert (:db master-cf)
                  (:coll master-cf)
                  {:worker name :data args})))
@@ -75,7 +100,7 @@
 ;;; job
 ;;;
 
-(defn start-jobs [pool pool-size job-ch]
+(defn- start-jobs [pool pool-size job-ch]
   (log/infof "run-jobs")
   (iter* (foreach job (range pool-size))
          (go-loop []
@@ -97,22 +122,57 @@
 ;;; Queue
 ;;;
 
-(defn pop-payload []
+(def ^:private cron-checked (atom 0))
+
+(defn- pop-payload []
   (mc/find-and-modify (:db master-cf)
                       (:coll master-cf)
                       {:state nil}
                       {$set {:state "running"} $inc {:tries 1}}
                       {:sort {:_id 1}}))
 
-(defn run-queue [pool-size job-ch]
+(defn- pop-cron []
+  (let [now (time/now)
+        window (time/plus now (time/millis (:cron-window-ms master-cf)))]
+    (when-let [payload (mc/find-and-modify (:db master-cf)
+                                           (:cron master-cf)
+                                           {:next {$lt now}}
+                                           {$set {:next window}}
+                                           {})]
+      (if (not (@workers (:name payload)))
+          (do
+            (log/warnf "No cron worker %s registered, removing job" (:name payload))
+            (mc/remove (:db master-cf) (:cron master-cf) {:_id (:_id payload)})
+            (pop-cron))
+          (let [next (time/plus (:next payload) (time/millis (:interval payload)))]
+            (mc/insert (:db master-cf)
+                       (:coll master-cf)
+                       (assoc payload :state "running" :tries 1))
+            (mc/find-and-modify (:db master-cf)
+                                (:cron master-cf)
+                                {:_id (:_id payload)}
+                                {$set {:next next}}
+                                {})
+            payload)))))
+
+(defn- pop-queue []
+  (or (when (> (System/currentTimeMillis) (+ @cron-checked (:cron-ms master-cf)))
+        (if-let [payload (pop-cron)]
+            payload
+            (do
+              (reset! cron-checked (System/currentTimeMillis))
+              nil)))
+      (pop-payload)))
+
+(defn- run-queue [pool-size job-ch]
   (try
     (iter* (times)
-           (>!! job-ch :ping)        ; Find available job runner
+           (>!! job-ch :ping)           ; Find available job runner
            (iter* (times)
-                  (with payload (pop-payload))
-                  (if (not payload)
+                  (with payload (pop-queue)) ; Pop from the queue
+                  (if (not payload)     ; Sleep if queue is empty
                       (Thread/sleep (:poll-ms master-cf))
-                      (do
+                      (do               ; Send the job to be processed
                         (>!! job-ch (select-keys payload [:data :worker]))
                         (break)))))
     (finally
@@ -141,3 +201,9 @@
        (register ~nm (fn ~args ~@body))
        (defn ~name [param#]
          (perform ~nm param#)))))
+
+(defmacro define-cron [name interval args & body]
+  (assert (= args []) "Cron function takes no arguments")
+  (let [nm (str *ns* "/" name)]
+    `(do
+       (register-cron ~nm ~interval (fn ~args ~@body)))))
