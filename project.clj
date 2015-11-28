8cdcf43ade099d991e4674bc974267fbdb28cfe3
Chris Dean <ctdean@sokitomi.com>
2015-11-28 00:17:21 -0800

    Do not aot compile anything
    This prevents the precomplied iter from losing the registered iter
    macros table

--- project.clj
+++ project.clj
@@ -1,25 +1,21 @@
 (defproject ctdean/backtick
-  "0.2.1"
+  "0.3.1"
   :description "Background job processing for Clojure using Postgres"
-  :main backtick.core
   :dependencies
-    [
-     [cheshire "5.5.0"]
-     [clams "0.2.6" :exclusions [ring]]
-     [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
-     [ctdean/iter "0.8.1"]
-     [org.clojure/clojure "1.7.0"]
-     [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-     [org.clojure/java.jdbc "0.4.2"]
-     [org.postgresql/postgresql "9.3-1102-jdbc41"]
-     [org.clojure/test.check "0.7.0"]
-     [org.clojure/tools.logging "0.3.1"]
-     [org.slf4j/slf4j-log4j12 "1.7.7"]
-     [yesql "0.5.1"]]
-    :plugins [
-              [com.jakemccrary/lein-test-refresh "0.10.0"]
-              ]
-    :profiles {
-               :test {:jvm-opts ["-Dclams.env=test"]}
-               :production {:jvm-opts ["-Dclams.env=production"]}
-               })
+  [
+   [cheshire "5.5.0"]
+   [clams "0.2.6" :exclusions [ring]]
+   [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
+   [ctdean/iter "0.9.0"]
+   [org.clojure/clojure "1.7.0"]
+   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+   [org.clojure/java.jdbc "0.4.2"]
+   [org.postgresql/postgresql "9.3-1102-jdbc41"]
+   [org.clojure/test.check "0.7.0"]
+   [org.clojure/tools.logging "0.3.1"]
+   [org.slf4j/slf4j-log4j12 "1.7.7"]
+   [yesql "0.5.1"]
+   ]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]
+  :profiles {:test {:jvm-opts ["-Dclams.env=test"]}
+             :production {:jvm-opts ["-Dclams.env=production"]}})
