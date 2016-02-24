249cbdef5d9f254630e5e17d9d90600f9a5bd600
Jim Brusstar <jim.brusstar@gmail.com>
2016-02-24 10:56:17 -0800

    New core interface
    Creates a clearer distinction between workers, jobs, registration and
    scheduling. Consistent conventions for each.

--- test/backtick/test/core_test.clj
+++ test/backtick/test/core_test.clj
@@ -12,36 +12,21 @@
       (is (= tag (cf)))
       (is (nil? (get (registered-workers) missing))))))
 
-(deftest register-cron-test
-  (let [nm (str "test-worker-" (rand-int 100))
-        missing (str "missing-worker-" (rand-int 100))
-        tag (rand-int 100)]
-    ;; new job
-    (register-cron nm 98700 (fn [] tag))
-    (let [cf (get (registered-workers) nm)
-          cron1 (get (registered-crons) nm)]
-      (is (= tag (cf)))
-      (is (nil? (get (registered-workers) missing)))
-      (is (= 98700 (:interval cron1))))
-    ;; new interval
-    (register-cron nm 98701 (fn [] (inc tag)))
-    (let [cf (get (registered-workers) nm)
-          cron2 (get (registered-crons) nm)]
-      (is (= (inc tag) (cf)))
-      (is (= 98701 (:interval cron2)))
-      ;; same interval
-      (register-cron nm 98701 (fn [] (+ tag 2)))
-      (let [cf (get (registered-workers) nm)
-            cron3 (get (registered-crons) nm)]
-        (is (= (+ tag 2) (cf)))
-        (is (= cron2 cron3))))))
+(deftest registered-workers-test
+  (with-redefs [backtick.engine/workers (atom {})]
+    (let [f1 (fn [] :foo)
+          f2 (fn [] :bar)]
+      (register "foo" f1)
+      (register "bar" f2)
+      (is (= {"foo" f1 "bar" f2}
+             (registered-workers))))))
 
 (def my-atom (atom nil))
 
 (define-worker my-test-worker [a b]
   (reset! my-atom (+ a b)))
 
-(define-cron my-test-cron (* 1234 100 60) []
+(define-recurring my-test-cron (* 1234 100 60) []
   (reset! my-atom 'cron))
 
 (defn schedule-test-job-0 []
@@ -56,3 +41,30 @@
   (register "schedule-test-job-1" schedule-test-job-1)
   (schedule schedule-test-job-1 88)
   (schedule my-test-worker 2 3))
+
+(deftest schedule-recurring-test
+  (let [nm (str "test-worker-" (rand-int 100))
+        missing (str "missing-worker-" (rand-int 100))
+        tag (rand-int 100)]
+    ;; new job
+    (register nm (fn [] tag))
+    (schedule-recurring 98700 nm)
+    (let [cf (get (registered-workers) nm)
+          cron1 (get (recurring-jobs) nm)]
+      (is (= tag (cf)))
+      (is (nil? (get (registered-workers) missing)))
+      (is (= 98700 (:interval cron1))))
+    ;; new interval
+    (register nm (fn [] (inc tag)))
+    (schedule-recurring 98701 nm)
+    (let [cf (get (registered-workers) nm)
+          cron2 (get (recurring-jobs) nm)]
+      (is (= (inc tag) (cf)))
+      (is (= 98701 (:interval cron2)))
+      ;; same interval
+      (register nm (fn [] (+ tag 2)))
+      (schedule-recurring 98701 nm)
+      (let [cf (get (registered-workers) nm)
+            cron3 (get (recurring-jobs) nm)]
+        (is (= (+ tag 2) (cf)))
+        (is (= cron2 cron3))))))
